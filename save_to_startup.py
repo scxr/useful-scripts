acd2b06cc5068a4c2a06d494bba473a9b3624e4c
cswil <56652842+cswil@users.noreply.github.com>
2019-10-17 14:56:46 +0100

    Update save_to_startup.py

--- save_to_startup.py
+++ save_to_startup.py
@@ -1,7 +1,6 @@
 import os
 fp="" #filepath to start
-uname=os.path.join(os.path.expandvars("%userprofile%"),"Documents and Settings").split("\\")
-os.chdir("C:/Users/"+uname[2]+"/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup")
+os.chdir("C:/Users/"+os.path.expandvars("%userprofile%").split("\\")[2]+"/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup")
 with open("start.bat","w") as god:
     god.write('start '+fp)
     god.close()
