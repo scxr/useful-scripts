dd326233f796e9133a564ee62fb13db78b0d95d2
cswil <56652842+cswil@users.noreply.github.com>
2019-10-19 21:25:27 +0100

    added comments

--- write_to_reg.py
+++ write_to_reg.py
@@ -1,13 +1,16 @@
+"""
+Desc : writes a key to registry that links to the file, no admin required
+"""
 import winreg as reg
 import os
 def wtr():
     pth=os.path.dirname(os.path.realpath(__file__))
     s_name="main.py"
-    address=os.path.join(pth,s_name)
-    key = reg.HKEY_CURRENT_USER
-    key_value = "Software\Microsoft\Windows\CurrentVersion\Run"
-    open = reg.OpenKey(key,key_value,0,reg.KEY_ALL_ACCESS) 
-    reg.SetValueEx(open,"any_name",0,reg.REG_SZ,address) 
-    reg.CloseKey(open) 
+    address=os.path.join(pth,s_name) # create the file path
+    key = reg.HKEY_CURRENT_USER # fetch the current users reg key
+    key_value = "Software\Microsoft\Windows\CurrentVersion\Run" # fetch curr key value
+    open = reg.OpenKey(key,key_value,0,reg.KEY_ALL_ACCESS) # open this reg kety
+    reg.SetValueEx(open,"any_name",0,reg.REG_SZ,address) # create my reg key and write it to the registry
+    reg.CloseKey(open) # close it 
 if __name__=="__main__":
-    wtr()
+    wtr() # run
